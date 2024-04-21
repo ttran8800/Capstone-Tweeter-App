a239023bbf2e3f1f89a4d80ac0807fd957859c5a
Thanh <ttran8800@gmail.com>
2024-04-21 16:14:38 -0500

    added config-server

---
+++ Config-Server/src/main/java/com/tweeter/Config/Server/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.tweeter.Config.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
