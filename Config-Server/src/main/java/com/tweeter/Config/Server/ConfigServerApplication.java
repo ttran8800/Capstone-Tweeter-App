71c661de221dc57c3abce44d82560dc98d47f72b
Thanh <ttran8800@gmail.com>
2024-04-21 19:49:55 -0500

    updated config-service, made it so server-register and mysql starts first, then config-server waits for them to be healthy before starting and user-service waits for config-server to be healthy before starting. everything is registered to eureka

--- Config-Server/src/main/java/com/tweeter/Config/Server/ConfigServerApplication.java
+++ Config-Server/src/main/java/com/tweeter/Config/Server/ConfigServerApplication.java
@@ -2,7 +2,12 @@ package com.tweeter.Config.Server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+
+@EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
